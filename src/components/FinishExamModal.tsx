65f735a51f5f059e63d210e9fedcb1e5aa125b4b
endingofourlife <endingofourlife@gmail.com>
2025-10-31 17:28:28 +0200

    added finish exam modal. fixed prev modals

--- src/components/FinishExamModal.tsx
+++ src/components/FinishExamModal.tsx
@@ -1,5 +1,10 @@
 import type {FormEvent} from "react";
 import {useNavigate} from "react-router-dom";
+import styles from './FinishExamModal.module.css';
+import lightIcon from '../../public/lightIcon.svg';
+import wrongIcon from '../../public/wrongIcon.svg';
+import correctIcon from '../../public/correctIcon.svg';
+import houseIcon from '../../public/homeIcon.png';
 
 interface FinishExamModalProps {
     xpEarned: number;
@@ -16,20 +21,49 @@ function FinishExamModal({xpEarned, successPercent}: FinishExamModalProps) {
     }
 
     return (
-        <dialog open={true}>
-            <h3>Exam results</h3>
-            <dl>
-                <dt>XP earned</dt>
-                <dd>{xpEarned}</dd>
+        <dialog open={true} className={styles.overlay}>
+            <form className={styles.modal} method="dialog">
+                <section className={styles.keepPracticing}>
+                    <h3>Keep Practicing!</h3>
+                    <p>Exam completed!</p>
+                     <div className={styles.circle}>
+                         <h4>{successPercent}%</h4>
+                         <p>Accuracy</p>
+                     </div>
+                    <div className={styles.resultsContainer}>
+                        <article className={styles.resultItem}>
+                            <img src={lightIcon} alt="xp-count-icon"/>
+                            <h4>+{xpEarned}</h4>
+                            <p>XP</p>
+                        </article>
 
-                <dt>Success percent</dt>
-                <dd>{successPercent}%</dd>
-            </dl>
-            <menu>
-                <button type={"button"} formMethod={"dialog"} onClick={handleClose}>
-                    Go to profile
-                </button>
-            </menu>
+                        <article className={styles.resultItem}>
+                            <img src={correctIcon} alt="correct-answers-icon"/>
+                            <h4>25</h4>
+                            <p>Correct</p>
+                        </article>
+
+                        <article className={styles.resultItem}>
+                            <img src={wrongIcon} alt="wrong-answers-icon"/>
+                            <h4>5</h4>
+                            <p>Wrong</p>
+                        </article>
+                    </div>
+                </section>
+
+                    <button type={"button"}
+                            formMethod={"dialog"}
+                            onClick={handleClose}
+                            className={styles.navigation}
+                    >
+                        <img src={houseIcon} alt="house-icon"/>
+                        Home
+                    </button>
+
+                <footer>
+                    <p>Practice makes perfect. Try reviewing the topics and test again!</p>
+                </footer>
+            </form>
         </dialog>
     );
 }
